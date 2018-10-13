a5966b5aab62ab4adcd52aea12371aa589cc876f
Jeremiah Biard <jeremiahbiard@gmail.com>
2018-10-13 06:21:00 -0400

    Initial commit

---
+++ app/src/main/java/net/excession/mathgame/GameActivity.java
@@ -0,0 +1,15 @@
+package net.excession.mathgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        // Initial
+    }
+}
---
+++ app/src/main/java/net/excession/mathgame/MainActivity.java
@@ -0,0 +1,12 @@
+package net.excession.mathgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
